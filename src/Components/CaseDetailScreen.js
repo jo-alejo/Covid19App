a6dbcd6650da33f404d7770eb045db0b2653066f
JalejoCikume <jalejo@cikume.com>
2020-03-26 15:30:32 -0600

    Commit 26.03.20 -1 Map Component added

--- src/Components/CaseDetailScreen.js
+++ src/Components/CaseDetailScreen.js
@@ -1,10 +1,9 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import {Container, Text, Card, CardItem} from 'native-base';
-import {StyleSheet, ActivityIndicator} from 'react-native';
 import {connect} from 'react-redux';
 import {caseUpdate} from '../Actions';
-import MapView, {Circle, PROVIDER_GOOGLE} from 'react-native-maps';
+import Map from './Map';
 
 class CaseDetailScreen extends Component {
   componentDidMount() {
@@ -13,30 +12,10 @@ class CaseDetailScreen extends Component {
     });
   }
 
-  renderMap(Reg) {
-    return (
-      <MapView style={styles.mapView} provider={PROVIDER_GOOGLE} region={Reg}>
-        <Circle
-          center={Reg}
-          radius={25}
-          strokeColor="rgba(158,158,255,1.0)"
-          fillColor="rgba(158,158,255,0.3)"
-        />
-      </MapView>
-    );
-  }
-
   render() {
-    const Region = {
-      latitude: parseFloat(this.props.case.Lat),
-      longitude: parseFloat(this.props.case.Lon),
-      latitudeDelta: 0.01,
-      longitudeDelta: 0.01,
-    };
-    console.log(Region);
     return (
       <Container>
-        {Region ? this.renderMap(Region) : <ActivityIndicator size="large" />}
+        <Map {...this.props} />
         <Card transparent>
           <CardItem>
             <Text>Code Case: {this.props.case.patientCode}</Text>
@@ -75,15 +54,6 @@ const mapStateToProps = state => {
     Lon,
   };
 };
-
-const styles = StyleSheet.create({
-  mapView: {
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-});
 
 // eslint-disable-next-line prettier/prettier
 export default connect(mapStateToProps, {caseUpdate})(CaseDetailScreen);
---
+++ src/Components/Map.js
@@ -0,0 +1,78 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {caseUpdate} from '../Actions';
+import MapView, {Circle, PROVIDER_GOOGLE} from 'react-native-maps';
+import {StyleSheet} from 'react-native';
+
+class Map extends Component {
+  componentDidMount() {
+    _.each(this.props.case, (value, prop) => {
+      this.props.caseUpdate({prop, value});
+    });
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: parseFloat(this.props.case.Lat),
+        longitude: parseFloat(this.props.case.Lon),
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+
+  onRegionChange(region) {
+    this.setState({region});
+  }
+
+  render() {
+    return (
+      <MapView
+        style={styles.mapView}
+        provider={PROVIDER_GOOGLE}
+        onRegionChange={this.onRegionChange.bind(this)}
+        initialRegion={this.state.region}>
+        <Circle
+          center={this.state.region}
+          radius={25}
+          strokeColor="rgba(158,158,255,1.0)"
+          fillColor="rgba(158,158,255,0.3)"
+        />
+      </MapView>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const {
+    patientCode,
+    patientName,
+    patientAge,
+    pClassification,
+    Lat,
+    Lon,
+  } = state.caseForm;
+  return {
+    patientCode,
+    patientName,
+    patientAge,
+    pClassification,
+    Lat,
+    Lon,
+  };
+};
+
+const styles = StyleSheet.create({
+  mapView: {
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+});
+
+// eslint-disable-next-line prettier/prettier
+export default connect(mapStateToProps, {caseUpdate})(Map);
